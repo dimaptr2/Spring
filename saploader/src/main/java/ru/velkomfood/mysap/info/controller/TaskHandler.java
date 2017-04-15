7fbf188d1177443710eab4e610b2b3b4bfe2dbb1
dpetrov <dpetrov@fclient.org>
2017-04-15 11:16:21 +0300

    SAP loader

---
+++ saploader/src/main/java/ru/velkomfood/mysap/info/controller/TaskHandler.java
@@ -0,0 +1,10 @@
+package ru.velkomfood.mysap.info.controller;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by dpetrov on 15.04.17.
+ */
+@Component
+public class TaskHandler {
+}
